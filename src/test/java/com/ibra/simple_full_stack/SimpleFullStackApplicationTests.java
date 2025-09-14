a1f831d470f68119c1ce39376db87533812a010c
Surface Book <iasafianu@st.ug.edu.gh>
2025-09-14 12:22:22 +0000

    feat: add test containers for db test

--- src/test/java/com/ibra/simple_full_stack/SimpleFullStackApplicationTests.java
+++ src/test/java/com/ibra/simple_full_stack/SimpleFullStackApplicationTests.java
@@ -2,9 +2,26 @@ package com.ibra.simple_full_stack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 class SimpleFullStackApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+			.withDatabaseName("testdb")
+			.withUsername("test")
+			.withPassword("test");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
